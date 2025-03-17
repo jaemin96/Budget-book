c542c9dacaf25ac0520115bb32fcabddb4bda532
jaemin96 <lemon__96@naver.com>
2025-03-17 15:59:45 +0900

    refactor: migration version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
