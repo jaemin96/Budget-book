7232168a91d2c9a39313b8abb29747d281782dc8
jaemin96 <lemon__96@naver.com>
2025-03-20 14:58:21 +0900

    fix: export name 수정 - counterReducer -> counter

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counter';
+import counter from './counter';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter,
   },
 });
 
