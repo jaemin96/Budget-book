c824e4f68a4baad553dca70a218bd58c82af1ed7
jaemin <77824886+jaemin96@users.noreply.github.com>
2025-04-28 23:20:52 +0900

    R1: 레포 통합 (모노레포 서버 & 클라이언트 통합) (#4)

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
