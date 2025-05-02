25dac3cfa4952047ead7194d230cdb54c4a26fcf
jaemin96 <lemon__96@naver.com>
2025-05-02 17:46:06 +0900

    [SERVER] chore(port): update proxy setting - client port

--- packages/bbserver/src/main.ts
+++ packages/bbserver/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3005',
+    origin: 'http://localhost:3030',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
