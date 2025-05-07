7d1ea97a5bb2a3b49c3060752ad346ce8f4b8dd8
jaemin96 <lemon__96@naver.com>
2025-05-07 10:52:21 +0900

    [SERVER] chore: migration prisma db schema

--- packages/bbserver/prisma/migrations/20250507011302_init/migration.sql
+++ packages/bbserver/prisma/migrations/20250507011302_init/migration.sql
@@ -2,10 +2,10 @@
 CREATE TYPE "TransactionType" AS ENUM ('INCOME', 'EXPENSE');
 
 -- CreateEnum
-CREATE TYPE "TransactionCategory" AS ENUM ('FOOD', 'TRANSPORT', 'UTILITIES', 'ETC');
+CREATE TYPE "TransactionCategory" AS ENUM ('SALARY', 'INSURANCE', 'DRINK', 'FOOD', 'SHOPPING', 'TRANSPORT', 'SUBSCRIBE', 'PHONE', 'DUES', 'PRESENT', 'SAVINGS', 'INVESTMENT', 'EMERGENCY_FUND', 'ETC');
 
 -- CreateEnum
-CREATE TYPE "TransactionWithdrawType" AS ENUM ('CARD', 'CASH', 'TRANSFER');
+CREATE TYPE "TransactionPaymentType" AS ENUM ('CREDIT_CARD', 'CHECK_CARD', 'KAKAO_PAY', 'APPLE_PAY', 'NAVER_PAY', 'TOSS', 'BANK_TRANSFER', 'CASH', 'POINT', 'GIFT_CARD', 'VIRTUAL_ACCOUNT', 'CRYPTO', 'ETC');
 
 -- CreateTable
 CREATE TABLE "Transaction" (
@@ -15,10 +15,9 @@ CREATE TABLE "Transaction" (
     "description" TEXT NOT NULL,
     "type" "TransactionType" NOT NULL,
     "category" "TransactionCategory" DEFAULT 'ETC',
-    "withdrawType" "TransactionWithdrawType",
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "paymentType" "TransactionPaymentType",
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
- 
