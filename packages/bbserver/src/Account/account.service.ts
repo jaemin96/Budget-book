3dbb88d1aec2fbf26969b55ef99d18a99d66c03a
jaemin96 <lemon__96@naver.com>
2025-05-12 14:58:00 +0900

    [SERVER] feat(account): add api - create and get account

--- packages/bbserver/src/Account/account.service.ts
+++ packages/bbserver/src/Account/account.service.ts
@@ -1,15 +1,34 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+  GetAccountInput,
+  GetAccountListInput,
+  GetAccountListOutput,
+  GetAccountOutput,
+} from './dto';
 import { PrismaService } from '../Prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AccountService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  /**
+   * 계좌 추가
+   */
+  async createAccount(input: CreateAccountInput): Promise<CreateAccountOutput> {
+    const account = await this.prisma.account.create({
+      data: input,
+    });
+
+    return { id: account.id };
+  }
 
   /**
    * 계좌 단건 조회
    */
-  async getAccount(input: any): Promise<any> {
+  async getAccount(input: GetAccountInput): Promise<GetAccountOutput> {
     const account = await this.prisma.account.findUnique({
       where: { bankName: input.bankName } as Prisma.AccountWhereUniqueInput,
     });
@@ -18,26 +37,43 @@ export class AccountService {
       throw new HttpException('해당 계좌가 존재 하지 않습니다.', HttpStatus.NOT_FOUND);
     }
 
-    return { account };
+    const sanitizedAccount = {
+      ...account,
+      totalBalance: account.totalBalance.toNumber(),
+      availableBalance: account.availableBalance.toNumber(),
+      savingBalance: account.savingBalance.toNumber(),
+      fixedDepositBalance: account.fixedDepositBalance.toNumber(),
+      investmentBalance: account.investmentBalance.toNumber(),
+      holdBalance: account.holdBalance.toNumber(),
+    };
+
+    return { account: sanitizedAccount };
   }
 
   /**
    * 전체 계좌 조회
    */
-  async getAccountList(input?: any): Promise<any> {
+  async getAccountList(input?: GetAccountListInput): Promise<GetAccountListOutput> {
     const where: Prisma.AccountWhereInput = {
-    //   type: input?.type ?? undefined,
+      //   type: input?.type ?? undefined,
     };
 
     const accounts = await this.prisma.account.findMany();
 
-    console.log(accounts);
+    const sanitizedAccounts = accounts.map((account) => ({
+      ...account,
+      totalBalance: account.totalBalance.toNumber(),
+      availableBalance: account.availableBalance.toNumber(),
+      savingBalance: account.savingBalance.toNumber(),
+      fixedDepositBalance: account.fixedDepositBalance.toNumber(),
+      investmentBalance: account.investmentBalance.toNumber(),
+      holdBalance: account.holdBalance.toNumber(),
+    }));
 
     if (!accounts || accounts.length === 0) {
       return { accounts: [] };
     }
 
-    return { accounts };
+    return { accounts: sanitizedAccounts };
   }
-
 }
