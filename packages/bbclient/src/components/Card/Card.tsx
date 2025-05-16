06b4f009e55036e64dc782d6895af82bde73724b
jaemin96 <lemon__96@naver.com>
2025-05-16 11:43:11 +0900

    [CLIENT] feat(card): add card compoennt props option - direction

--- packages/bbclient/src/components/Card/Card.tsx
+++ packages/bbclient/src/components/Card/Card.tsx
@@ -2,8 +2,14 @@ import { ReactNode } from "react";
 import styles from "./styles/card.module.scss";
 import classNames from "classnames";
 import { Disc, LucideIcon } from "lucide-react";
+import { Direction } from "@/common/types";
 interface CardComposition {
-  Header: React.FC<{ icon?: LucideIcon; title: string; buttons?: ReactNode }>;
+  Header: React.FC<{
+    icon?: LucideIcon;
+    title: string;
+    buttons?: ReactNode;
+    direction?: Direction;
+  }>;
   Body: React.FC<{ children: ReactNode }>;
 }
 
@@ -13,8 +19,13 @@ const Card: React.FC<{ children: ReactNode }> & CardComposition = ({
   return <div className={classNames(styles.card)}>{children}</div>;
 };
 
-Card.Header = ({ icon: Icon = Disc, title, buttons }) => (
-  <div className={classNames(styles.header)}>
+Card.Header = ({
+  icon: Icon = Disc,
+  title,
+  buttons,
+  direction = "horizontal",
+}) => (
+  <div className={classNames(styles.header, styles[`header--${direction}`])}>
     <div className={classNames(styles.titleWrapper)}>
       <Icon className={classNames(styles.icon)} />
       <span className={classNames(styles.title)}>{title}</span>
