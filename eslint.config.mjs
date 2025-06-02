26c403a326e70608c8199657b842fd096fa80276
jaemin96 <lemon__96@naver.com>
2025-06-02 13:09:30 +0900

    eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,32 +2,51 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import * as globals from "globals";
 import tseslint from "typescript-eslint";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
+  eslintPluginPrettierRecommended,
   {
     ignores: ["**/dist/**", "**/node_modules/**"],
-  },
-  {
-    extends: ["prettier"],
+    rules: {
+      quotes: "off",
+      // "prettier/prettier": [
+      //   "warn",
+      //   {
+      //     printWidth: 120,
+      //     bracketSameLine: false,
+      //   },
+      // ],
+      "prettier/prettier": "off", // prettier lint 끄기
+    },
   },
 
   // Server (bbserver) 설정
   {
     files: ["packages/bbserver/**/*.{ts,js}"],
     languageOptions: {
+      parser: tseslint.parser,
       globals: {
         ...globals.node,
         ...globals.jest,
       },
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: path.resolve("packages/bbserver"), // <-- 고쳤음
-        project: ["./tsconfig.json"], // <-- 추가
+        tsconfigRootDir: path.resolve(__dirname, "packages", "bbserver"),
+        project: [path.resolve(__dirname, "/tsconfig.json")],
       },
       sourceType: "commonjs",
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
+    },
+    settings: {
+      prettier: {
+        configPath: path.resolve(__dirname, "prettier.config.mjs"),
+      },
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
@@ -40,24 +59,23 @@ export default tseslint.config(
   {
     files: ["packages/bbclient/**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: [path.resolve(__dirname, "packages/bbclient/tsconfig.json")],
+        tsconfigRootDir: path.resolve(__dirname, "packages", "bbclient"),
+      },
       globals: {
         ...globals.browser,
       },
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: path.resolve("packages/bbclient"),
-        project: ["./tsconfig.json"], // <-- 추가
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint.plugin, // <-- 일관성 위해 추가 (선택)
     },
     settings: {
       next: {
-        rootDir: path.resolve("packages/bbclient"),
+        rootDir: path.resolve(__dirname, "packages", "bbclient"),
+      },
+      prettier: {
+        configPath: path.resolve(__dirname, "/prettier.config.mjs"),
       },
     },
-  },
-
-  eslintPluginPrettierRecommended
+    // eslint-disable-next-line prettier/prettier
+  }
 );
