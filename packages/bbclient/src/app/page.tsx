5d995ca9f9d178105ea71c138db8cc88da761b81
jaemin96 <lemon__96@naver.com>
2025-06-11 13:19:14 +0900

    [CLIENT] fix(loading): add loading spinner - amount list

--- packages/bbclient/src/app/page.tsx
+++ packages/bbclient/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import styles from "./styles/home.module.scss";
 import Link from "next/link";
-import { Button, Card } from "@/components";
+import { Button, Card, Spinner } from "@/components";
 import { useQuery } from "@apollo/client";
 import { GET_AMOUNT_SUMMARY } from "../graphql/queries/Account";
 import { useEffect, useState } from "react";
@@ -31,45 +31,56 @@ export default function Home() {
         </Link>
       </Card>
 
-      {/* 총 금액 */}
-      <section>
-        <Card>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h3>{`Total amount`}</h3>
-            <span>{`${summary?.totalBalance.toLocaleString()}`}</span>
+      {!summary ? (
+        <>
+          <div style={{ display: "flex", gap: "0.4rem" }}>
+            <span style={{ color: "white" }}>{`금액 현황 불러오는중 ...`}</span>
+            <Spinner />
           </div>
-        </Card>
-      </section>
+        </>
+      ) : (
+        <>
+          {/* 총 금액 */}
+          <section>
+            <Card>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>{`Total amount`}</h3>
+                <span>{`${summary?.totalBalance.toLocaleString()}`}</span>
+              </div>
+            </Card>
+          </section>
 
-      {/* 저축 금액 */}
-      <section>
-        <Card>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h3>{`Saving amount`}</h3>
-            <span>{`${summary?.savingBalance.toLocaleString()}`}</span>
-          </div>
-        </Card>
-      </section>
+          {/* 저축 금액 */}
+          <section>
+            <Card>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>{`Saving amount`}</h3>
+                <span>{`${summary?.savingBalance.toLocaleString()}`}</span>
+              </div>
+            </Card>
+          </section>
 
-      {/* 바로 출금 가능 금액 */}
-      <section>
-        <Card>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h3>{`Available amount`}</h3>
-            <span>{`${summary?.availableBalance.toLocaleString()}`}</span>
-          </div>
-        </Card>
-      </section>
+          {/* 바로 출금 가능 금액 */}
+          <section>
+            <Card>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>{`Available amount`}</h3>
+                <span>{`${summary?.availableBalance.toLocaleString()}`}</span>
+              </div>
+            </Card>
+          </section>
 
-      {/* 출금 예정 금액  */}
-      <section>
-        <Card>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h3>{`Holding amount`}</h3>
-            <span>{`${summary?.holdBalance.toLocaleString()}`}</span>
-          </div>
-        </Card>
-      </section>
+          {/* 출금 예정 금액  */}
+          <section>
+            <Card>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>{`Holding amount`}</h3>
+                <span>{`${summary?.holdBalance.toLocaleString()}`}</span>
+              </div>
+            </Card>
+          </section>
+        </>
+      )}
     </div>
   );
 }
