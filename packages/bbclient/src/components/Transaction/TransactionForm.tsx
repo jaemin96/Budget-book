edd9e56f26a24822c45176c58aa0cc60fcfd0b78
jaemin96 <lemon__96@naver.com>
2025-07-02 00:25:56 +0900

    [CLIENT] feat(Link): add Link Button - go transactions (#21)
    - 트랜잭션 추가 페이지에 목록 이동 버튼 추가
    - 트랜잭션 추가 페이지 헤더영역 스타일 작업
    
    Refs #21

--- packages/bbclient/src/components/Transaction/TransactionForm.tsx
+++ packages/bbclient/src/components/Transaction/TransactionForm.tsx
@@ -19,6 +19,8 @@ import LoadingSpinner from "@/components/Loading/Spinner";
 import { Form, useForm, Input, Button } from "@/components";
 import { FormMode } from "@/common/types";
 import { RadioGroup, Radio, Select, Textarea } from "../Form/fields";
+import Link from "next/link";
+import { ArrowLeftIcon, Octagon } from "lucide-react";
 
 export interface TransactionFormProps {
   mode: FormMode;
@@ -92,69 +94,87 @@ const TransactionForm: React.FC<TransactionFormProps> = ({
   }, [data]);
 
   return (
-    <div className={classNames(styles["transaction-form-wrapper"])}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <Form.Item label="금액">
-          <Input name="amount" type="number" value={init && init.amount} />
-        </Form.Item>
-
-        <Form.Item label="거래자">
-          <Input name="depositor" type="text" value={init && init.depositor} />
-        </Form.Item>
-
-        <Form.Item label="거래 유형" name="type">
-          <RadioGroup name="type" value={selected} onChange={setSelected}>
-            <Radio value="EXPENSE">지출</Radio>
-            <Radio value="INCOME">수익</Radio>
-            <Radio value="TRANSFER">내 계좌 간 거래</Radio>
-          </RadioGroup>
-        </Form.Item>
-
-        {selected === "TRANSFER" ? (
-          <div className="myTransfer">
-            <Form.Item label="보낼 계좌" name="fromAccountId">
-              <Select name="fromAccountId" value={init?.fromAccountId}>
-                {ACCOUNTS.map(({ value, label }) => (
-                  <Select.Option key={value} value={value}>
-                    {label}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-
-            <Form.Item label="받을 계좌" name="toAccountId">
-              <Select name="toAccountId" value={init?.toAccountId}>
-                {ACCOUNTS.map(({ value, label }) => (
-                  <Select.Option key={value} value={value}>
-                    {label}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
+    <>
+      <div className={classNames(styles["transaction-form-wrapper"])}>
+        <div className={classNames(styles["transaction-form-wrapper-header"])}>
+          <div className={classNames(styles["header-title-wrapper"])}>
+            <Octagon className={classNames(styles["icon"])} />
+            <span className={classNames(styles["title"])}>
+              {"Create Transaction"}
+            </span>
           </div>
-        ) : selected === "INCOME" ? (
-          <Form.Item label="수령 계좌" name="accountId">
-            <Select name="accountId" value={init?.toAccountId}>
-              {ACCOUNTS.map(({ value, label }) => (
-                <Select.Option key={value} value={value}>
-                  {label}
-                </Select.Option>
-              ))}
-            </Select>
+          <Button>
+            <Link className={styles.link} href="/transaction">
+              <ArrowLeftIcon className={classNames(styles["icon"])} />
+            </Link>
+          </Button>
+        </div>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Form.Item label="금액">
+            <Input name="amount" type="number" value={init && init.amount} />
           </Form.Item>
-        ) : (
-          <Form.Item label="사용 계좌" name="accountId">
-            <Select name="accountId" value={init?.fromAccountId}>
-              {ACCOUNTS.map(({ value, label }) => (
-                <Select.Option key={value} value={value}>
-                  {label}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-        )}
 
-        {/* <Form.Item label="계좌 항목" name="accountField">
+          <Form.Item label="거래자">
+            <Input
+              name="depositor"
+              type="text"
+              value={init && init.depositor}
+            />
+          </Form.Item>
+
+          <Form.Item label="거래 유형" name="type">
+            <RadioGroup name="type" value={selected} onChange={setSelected}>
+              <Radio value="EXPENSE">지출</Radio>
+              <Radio value="INCOME">수익</Radio>
+              <Radio value="TRANSFER">내 계좌 간 거래</Radio>
+            </RadioGroup>
+          </Form.Item>
+
+          {selected === "TRANSFER" ? (
+            <div className="myTransfer">
+              <Form.Item label="보낼 계좌" name="fromAccountId">
+                <Select name="fromAccountId" value={init?.fromAccountId}>
+                  {ACCOUNTS.map(({ value, label }) => (
+                    <Select.Option key={value} value={value}>
+                      {label}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Form.Item>
+
+              <Form.Item label="받을 계좌" name="toAccountId">
+                <Select name="toAccountId" value={init?.toAccountId}>
+                  {ACCOUNTS.map(({ value, label }) => (
+                    <Select.Option key={value} value={value}>
+                      {label}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </Form.Item>
+            </div>
+          ) : selected === "INCOME" ? (
+            <Form.Item label="수령 계좌" name="accountId">
+              <Select name="accountId" value={init?.toAccountId}>
+                {ACCOUNTS.map(({ value, label }) => (
+                  <Select.Option key={value} value={value}>
+                    {label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          ) : (
+            <Form.Item label="사용 계좌" name="accountId">
+              <Select name="accountId" value={init?.fromAccountId}>
+                {ACCOUNTS.map(({ value, label }) => (
+                  <Select.Option key={value} value={value}>
+                    {label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          )}
+
+          {/* <Form.Item label="계좌 항목" name="accountField">
           <select
             name="accountField"
             value={init && init.category}
@@ -168,50 +188,51 @@ const TransactionForm: React.FC<TransactionFormProps> = ({
           </select>
         </Form.Item> */}
 
-        <Form.Item label="거래 분류" name="category">
-          <Select name="category" value={init?.category}>
-            {CATEGORY_OPTIONS.map(({ value, label }) => (
-              <Select.Option key={value} value={value}>
-                {label}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
+          <Form.Item label="거래 분류" name="category">
+            <Select name="category" value={init?.category}>
+              {CATEGORY_OPTIONS.map(({ value, label }) => (
+                <Select.Option key={value} value={value}>
+                  {label}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
 
-        <Form.Item label="거래 수단" name="paymentType">
-          <Select name="paymentType" value={init?.paymentType}>
-            {PAYMENT_OPTIONS.map(({ value, label }) => (
-              <Select.Option key={value} value={value}>
-                {label}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
+          <Form.Item label="거래 수단" name="paymentType">
+            <Select name="paymentType" value={init?.paymentType}>
+              {PAYMENT_OPTIONS.map(({ value, label }) => (
+                <Select.Option key={value} value={value}>
+                  {label}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
 
-        <Form.Item label="거래 설명" name="description">
-          <Textarea name="description" value={init && init.description} />
-        </Form.Item>
+          <Form.Item label="거래 설명" name="description">
+            <Textarea name="description" value={init && init.description} />
+          </Form.Item>
 
-        <div style={{ width: "100%", textAlign: "right" }}>
-          {createLoading || updateLoading ? (
-            <>
-              <div style={{ width: "100%", textAlign: "center" }}>
-                <LoadingSpinner />
-              </div>
-            </>
-          ) : (
-            <>
-              <Button
-                className={styles[`transaction-submit-button`]}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </>
-          )}
-        </div>
-      </Form>
-    </div>
+          <div style={{ width: "100%", textAlign: "right" }}>
+            {createLoading || updateLoading ? (
+              <>
+                <div style={{ width: "100%", textAlign: "center" }}>
+                  <LoadingSpinner />
+                </div>
+              </>
+            ) : (
+              <>
+                <Button
+                  className={styles[`transaction-submit-button`]}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </>
+            )}
+          </div>
+        </Form>
+      </div>
+    </>
   );
 };
 
