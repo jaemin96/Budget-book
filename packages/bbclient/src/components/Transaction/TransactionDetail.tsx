65d2bf6bb5d5cac17f1ba310b89624fb6d19ba96
jaemin96 <lemon__96@naver.com>
2025-09-25 13:34:10 +0900

    [bbclient] feat(detail): upgrade transaction detail page (#23)
    - 거래 상세 페이지에서 정보 수정 기능 추가
    - 읽기전용, 수정전용 화면 분할
    - 데이터 패칭 및 리패치 처리
    
    Refs #23

--- packages/bbclient/src/components/Transaction/TransactionDetail.tsx
+++ packages/bbclient/src/components/Transaction/TransactionDetail.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { Octagon, ArrowLeftIcon } from "lucide-react";
-import { Card, Button, Spinner } from "@/components";
+import {
+  Card,
+  Button,
+  Spinner,
+  useForm,
+  Form,
+  Input,
+  RadioGroup,
+  Radio,
+  Select,
+  Textarea,
+} from "@/components";
 import Link from "next/link";
 import classNames from "classnames";
 import { GET_TRANSACTION } from "@/graphql/queries/Transaction";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import styles from "./styles/transaction.module.scss";
 import { useEffect, useState } from "react";
 import {
@@ -13,8 +24,10 @@ import {
   TransactionPaymentTypeLabels,
   TransactionCategoryLabels,
 } from "@/constants/enum";
-import { Transaction } from "@/model/transaction.model";
+import { Transaction, TransactionType } from "@/model/transaction.model";
 import { AccountBank } from "@/model/account.model";
+import { UPDATE_TRANSACTION } from "@/graphql/mutations/Transaction";
+import { ACCOUNTS, CATEGORY_OPTIONS, PAYMENT_OPTIONS } from "@/constants/data";
 
 interface TransactionDetailProps {
   transactionId: number;
@@ -28,7 +41,15 @@ const convertDate = (date: Date) => {
 export const TransactionDetail = ({
   transactionId,
 }: TransactionDetailProps) => {
-  const [tr, setTr] = useState<Transaction>();
+  const [tr, setTr] = useState<Transaction>(); // 현재 거래 정보
+  const [init, setInit] = useState<any>();
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [selectedType, setSelectedType] = useState<any>();
+  const { formRef, getValues } = useForm<any>();
+
+  const [updateMutation, { loading: updateLoading }] =
+    useMutation(UPDATE_TRANSACTION);
+
   const { data, loading, error, refetch } = useQuery(GET_TRANSACTION, {
     variables: {
       input: {
@@ -37,11 +58,44 @@ export const TransactionDetail = ({
     },
   });
 
+  const handleEditMode = () => {
+    setEditMode(true);
+  };
+
+  const handleReadMode = () => {
+    setEditMode(false);
+  };
+
+  const handleUpdate = async () => {
+    if (!transactionId) return;
+
+    try {
+      const values = getValues();
+      const params = { ...values, id: +transactionId };
+
+      await updateMutation({
+        variables: {
+          input: { ...params },
+        },
+      });
+
+      setEditMode(false);
+      refetch();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     if (!data?.getTransaction?.transaction) return;
 
     setTr(data?.getTransaction?.transaction);
+    setInit(data?.getTransaction?.transaction);
   }, [data]);
+
+  useEffect(() => {
+    setSelectedType(init?.type);
+  }, [init]);
 
   return (
     <Card>
@@ -61,13 +115,44 @@ export const TransactionDetail = ({
       <Card.Body>
         <div className={classNames(styles["transaction-summary-wrapper"])}>
           <div className={styles["transaction-header"]}>
-            거래내역 번호: {transactionId}
+            <span>거래내역 번호: {transactionId}</span>
+            <div className={styles["transaction-header-btn-wrapper"]}>
+              {editMode ? (
+                <>
+                  <Button
+                    className={styles["common-btn"]}
+                    buttonMode="primary"
+                    type="submit"
+                    onClick={handleUpdate}
+                  >
+                    저장
+                  </Button>
+                  <Button
+                    className={styles["common-btn"]}
+                    buttonMode="outline"
+                    onClick={handleReadMode}
+                  >
+                    취소
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    className={styles["common-btn"]}
+                    buttonMode="primary"
+                    onClick={handleEditMode}
+                  >
+                    수정
+                  </Button>
+                </>
+              )}
+            </div>
           </div>
 
           <div className={styles["transaction-content"]}>
             {!tr ? (
               <Spinner />
-            ) : (
+            ) : !editMode ? (
               <>
                 <TransactionItem
                   label="💳 결제수단"
@@ -107,6 +192,150 @@ export const TransactionDetail = ({
                 <TransactionItem label="🔁 거래흐름" value={tr.depositor} />
                 <TransactionItem label="🗒️ 설명" value={tr.description} />
               </>
+            ) : (
+              <div className={styles["detail-edit-wrapper"]}>
+                <Form ref={formRef} onSubmit={handleUpdate}>
+                  <Form.Item
+                    className={styles["detail"]}
+                    label="💳 결제수단"
+                    name="paymentType"
+                  >
+                    <Select name="paymentType" defaultValue={init?.paymentType}>
+                      {PAYMENT_OPTIONS.map(({ value, label }) => (
+                        <Select.Option key={value} value={value}>
+                          {label}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+
+                  <Form.Item className={styles["detail"]} label="💰 금액">
+                    <Input
+                      name="amount"
+                      defaultValue={init?.amount}
+                      type="number"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    className={styles["detail"]}
+                    label="📂 타입"
+                    name="type"
+                  >
+                    <RadioGroup
+                      name="type"
+                      value={selectedType}
+                      onChange={setSelectedType}
+                    >
+                      <Radio value="EXPENSE">지출</Radio>
+                      <Radio value="INCOME">수익</Radio>
+                      <Radio value="TRANSFER">내 계좌 간 거래</Radio>
+                    </RadioGroup>
+                  </Form.Item>
+
+                  {selectedType === "TRANSFER" ? (
+                    <div className="myTransfer">
+                      <Form.Item
+                        className={styles["detail"]}
+                        label="💳 보낼 계좌"
+                        name="fromAccountId"
+                      >
+                        <Select
+                          name="fromAccountId"
+                          defaultValue={init?.fromAccountId}
+                        >
+                          {ACCOUNTS.map(({ value, label }) => (
+                            <Select.Option key={value} value={value}>
+                              {label}
+                            </Select.Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+
+                      <Form.Item
+                        className={styles["detail"]}
+                        label="💳 받을 계좌"
+                        name="toAccountId"
+                      >
+                        <Select
+                          name="toAccountId"
+                          defaultValue={init?.toAccountId}
+                        >
+                          {ACCOUNTS.map(({ value, label }) => (
+                            <Select.Option key={value} value={value}>
+                              {label}
+                            </Select.Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    </div>
+                  ) : selectedType === "INCOME" ? (
+                    <Form.Item
+                      className={styles["detail"]}
+                      label="💳 수령 계좌"
+                      name="toAccountId"
+                    >
+                      <Select
+                        name="toAccountId"
+                        defaultValue={init?.toAccountId}
+                      >
+                        {ACCOUNTS.map(({ value, label }) => (
+                          <Select.Option key={value} value={value}>
+                            {label}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  ) : (
+                    <Form.Item
+                      className={styles["detail"]}
+                      label="💳 사용 계좌"
+                      name="fromAccountId"
+                    >
+                      <Select
+                        name="fromAccountId"
+                        defaultValue={init?.fromAccountId}
+                      >
+                        {ACCOUNTS.map(({ value, label }) => (
+                          <Select.Option key={value} value={value}>
+                            {label}
+                          </Select.Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  )}
+                  <Form.Item
+                    className={styles["detail"]}
+                    label="📝 카테고리"
+                    name="category"
+                  >
+                    <Select name="category" defaultValue={init?.category}>
+                      {CATEGORY_OPTIONS.map(({ value, label }) => (
+                        <Select.Option key={value} value={value}>
+                          {label}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                  <Form.Item className={styles["detail"]} label="🔁 거래흐름">
+                    <Input
+                      name="depositor"
+                      type="text"
+                      defaultValue={init && init.depositor}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    className={styles["detail"]}
+                    label="🗒️ 설명"
+                    name="description"
+                  >
+                    <Textarea
+                      name="description"
+                      defaultValue={init && init.description}
+                    />
+                  </Form.Item>
+                </Form>
+              </div>
             )}
           </div>
 
