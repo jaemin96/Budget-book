786bbca73d99959ec5fd3a3b30144ac8b24d2abf
jaemin96 <lemon__96@naver.com>
2025-10-14 17:31:06 +0900

    [CLIENT] feat(auth): add login process and check auth
    - add guard component - protected
    - update user login form (style and add UI)
    - update apolloClient config (add process token validation)
    
    Refs #26

--- packages/bbclient/src/app/authentication/components/UserAuthForm.tsx
+++ packages/bbclient/src/app/authentication/components/UserAuthForm.tsx
@@ -3,12 +3,36 @@
 import { useState } from "react";
 import styles from "./userAuthForm.module.scss";
 import inputStyles from "../../../components/Form/fields/styles/input.module.scss";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "@/graphql/mutations/Auth";
+import { Eye, EyeOff } from "lucide-react";
+import { Spinner } from "@/components";
+import { useRouter } from "next/navigation";
 
 export function UserAuthForm() {
+  const router = useRouter();
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const [loginMutation, { loading }] = useMutation(LOGIN);
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    try {
+      const result = await loginMutation({
+        variables: {
+          email,
+          password,
+        },
+      });
+
+      localStorage.setItem("token", result.data.login.accessToken);
+      router.replace("/");
+    } catch (err) {
+      console.error("Login error:", err);
+    }
   };
 
   return (
@@ -21,7 +45,43 @@ export function UserAuthForm() {
         onChange={(e) => setEmail(e.target.value)}
         required
       />
-      <button type="submit">Continue</button>
+      <div
+        style={{
+          position: "relative",
+        }}
+      >
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          className={inputStyles.input}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          style={{
+            position: "absolute",
+            right: 10,
+            top: "50%",
+            transform: "translateY(-50%)",
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {showPassword ? (
+            <EyeOff size={16} color={"gray"} />
+          ) : (
+            <Eye size={16} color={"gray"} />
+          )}
+        </button>
+      </div>
+      <button type="submit">{loading ? <Spinner /> : `LOGIN`}</button>
     </form>
   );
 }
--- packages/bbclient/src/lib/apolloClient.ts
+++ packages/bbclient/src/lib/apolloClient.ts
@@ -1,7 +1,55 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import Router from "next/router";
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  }));
+  return forward(operation);
+});
+
+const errorLink = onError(({ networkError, graphQLErrors }) => {
+  if (
+    networkError &&
+    "statusCode" in networkError &&
+    networkError.statusCode === 401
+  ) {
+    // 토큰 만료 또는 인증 실패 시
+    localStorage.removeItem("token");
+    Router.replace("/authentication");
+  }
+
+  // GraphQL 에러 중 인증 실패 처리
+  if (graphQLErrors) {
+    for (let err of graphQLErrors) {
+      console.log({ err });
+
+      if (err.extensions?.code === "UNAUTHENTICATED") {
+        localStorage.removeItem("token");
+        Router.replace("/authentication");
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
+  link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache(),
 });
 
